00fbd8e5863036979dcf6bd1a22b9ed6462ad80b
JuanFuentes <juanzito.12@gmail.com>
2021-10-27 21:56:09 -0300

    first commit

---
+++ desafios/desafio1.js
@@ -0,0 +1,13 @@
+var nombreDeUsuario = prompt("Ingrese su nombre");
+  console.log(`ingreso el nombre: ${nombreDeUsuario}`)
+
+var apellidoDeUsuario = prompt("Ingresu su apellido");
+  console.log(`su apellido es:${apellidoDeUsuario}`)
+
+var nombreCompleto = nombreDeUsuario + " " + apellidoDeUsuario;
+  console.log(`su nombre completo es:${nombreCompleto}`)
+
+var edadDeUsuario = parseInt (prompt ("Cual es tu edad?"));
+  console.log(`ingreso la edad de: ${edadDeUsuario}`)
+ 
+alert(`su nombre completo es: ${nombreCompleto} y su edad es ${edadDeUsuario}`)
